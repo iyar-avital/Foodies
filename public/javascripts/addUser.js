11905baa147fdd05fb2154de4da72f90cd84faba
iyar-avital <iyaravital@gmail.com>
2022-04-20 18:09:44 +0300

    Delete unneccesary output

--- public/javascripts/addUser.js
+++ public/javascripts/addUser.js
@@ -1,5 +1,4 @@
 async function addUser() {
-    console.log("arrives addUser");
     let addUserForm = document.getElementById('addUserForm');
     let data = new URLSearchParams(new FormData(addUserForm));
     await fetchData("/users/add", { method: 'post', body: data }, true);
