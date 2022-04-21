3ced7cb2e704964c15150041d5b4c53b8bd5e598
iyar-avital <iyaravital@gmail.com>
2022-04-21 14:21:04 +0300

    Add option to upload flower image by url

--- controllers/flowers_controller.js
+++ controllers/flowers_controller.js
@@ -1,4 +1,5 @@
 const service = require("../services/flowers_services.js");
+const IMAGE_PATH = 'images/';
 
 const flowersView = (req, res) => {
     res.render("flowers");
@@ -12,8 +13,8 @@ const flowersData = async (req, res) => {
 const addFlower = async (req, res) => {
     setTimeout(async function () {
         let body = req.body;
-        let file = req.file;
-        flower = [body.name, file.filename, body.color, body.price];
+        let imageUrl = req.file ? IMAGE_PATH + req.file.filename : body.imageUrl;
+        let flower = [body.name, imageUrl, body.color, body.price];
         let newFlower = await service.addFlower(flower);
         if (typeof newFlower !== "string") {
             res.sendStatus(200);
