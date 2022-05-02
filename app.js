a9d35ff74a91c16f5d21c73af01577fa19350780
iyar-avital <iyaravital@gmail.com>
2022-05-02 23:27:01 +0300

    Some changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/home_routes');
 var navigationRouter = require('./routes/navigation_routes');
@@ -14,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
