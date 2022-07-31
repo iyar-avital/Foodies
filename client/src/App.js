9e6bc1aa4e2bf4b8560655b0a625055ae9840e72
Rivkazi <zizovirivka@gmail.com>
2022-07-31 23:09:35 +0300

    App routes and settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,40 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import AppRouts from "./AppRouts";
+import { AppContext, socket } from "./context/appContext";
 
 function App() {
+  const [rooms, setRooms] = useState([]);
+  const [currentRoom, setCurrentRoom] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [newMessages, setNewMessages] = useState([]);
+
+  const [clients, setClients] = useState([]);
+  const [serviceMsg, setServiceMsg] = useState(false);
   return (
-    <div className="App">
-      <AppRouts />
-    </div>
+    <AppContext.Provider
+      value={{
+        socket,
+        rooms,
+        setRooms,
+        currentRoom,
+        setCurrentRoom,
+        messages,
+        setMessages,
+        newMessages,
+        setNewMessages,
+        clients,
+        setClients,
+        serviceMsg,
+        setServiceMsg,
+      }}
+    >
+      <div className="App">
+        <AppRouts />
+      </div>
+    </AppContext.Provider>
   );
 }
+
 export default App;
