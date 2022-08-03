93ec2154d15170890db07c62f26a7942db28c14b
iyar-avital <iyaravital@gmail.com>
2022-08-03 21:53:26 +0300

    Add private key to env file

--- server/utils/decryption.js
+++ server/utils/decryption.js
@@ -1,10 +1,9 @@
 const crypto = require("crypto");
-const fs = require("fs");
 const Buffer = require("buffer/").Buffer;
 
 const decrypt = (message) => {
   const privateKey = {
-    key: fs.readFileSync(process.env.CRYPT_PRIVET_KEY, "utf8"),
+    key: process.env.PRIVATE_KEY,
     passphrase: "",
     padding: crypto.constants.RSA_PKCS1_PADDING,
   };
