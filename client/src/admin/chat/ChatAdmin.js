1e84c96e79d7d8c341507b45cfd1b328dae0b066
iyar-avital <iyaravital@gmail.com>
2022-08-03 22:01:48 +0300

    Fix bugs

--- client/src/admin/chat/ChatAdmin.js
+++ client/src/admin/chat/ChatAdmin.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import MessageForm from "../../chat/MessageForm";
+import AuthAdminComp from "../../comps/auth/authAdminComp";
 import SideBarAdmin from "./SideBarAdmin";
 
 function ChatAdmin() {
   return (
     <div className="container-fluid chat__bg p-4">
+      <AuthAdminComp />
       <Container>
         <Row>
-          <Col md={4}>
+          <Col md={5}>
             <SideBarAdmin />
           </Col>
-          <Col md={8}>
+          <Col md={7}>
             <MessageForm />
           </Col>
         </Row>
